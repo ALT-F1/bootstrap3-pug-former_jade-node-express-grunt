d21a3a74b54199448d1503931f3d852dd44950d1
Ilya Sheershoff <sheershoff@gmail.com>
2016-08-30 15:18:21 +0500

    migrate to pug v2

--- app/app.js
+++ app/app.js
@@ -25,7 +25,7 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || PORT_LISTENER);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 app.use(favicon(__dirname + '/public/bootstrap-3.3.1/docs/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser({ keepExtensions: true, uploadDir: path.join(__dirname, appConfig.directories.publicDir) }));
